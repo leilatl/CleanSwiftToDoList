a0b4911090312d0f7ee04ee4712c79bf75e78080
Leila Tolegenova <leilatheleph@gmail.com>
2023-03-03 06:22:03 +0600

    Initial Commit

---
+++ ToDoListCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCleanSwift
+//
+//  Created by Dmitry Serebrov on 03.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
